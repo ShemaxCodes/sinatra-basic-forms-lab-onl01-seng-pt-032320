c1aff305cb49c5ffcafe5c1d9cdbd3c5b1a30613
Jashema Panapa <jcpanapa@gmail.com>
2020-06-18 19:27:00 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,7 +1,8 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
 
 class Puppy 
-  
+  attr_accessor :name, :breed, :age
+  attr_writer :name 
   def initialize 
     
     
